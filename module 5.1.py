db35237c49bb42620c18c42647fa246e8810bff1
Artem2104 <sozinov2104@gmail.com>
2024-11-07 15:23:21 +0300

    Update module 5.1.py

--- module 5.1.py
+++ module 5.1.py
@@ -1,14 +1,17 @@
-class House:
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+class House:                                                # 1
+    def __init__(self, name, number_of_floors):             # 2
+        self.name = name                                    # 3
+        self.number_of_floors = number_of_floors            # 3
 
-    def go_to(self, new_floor):
-        if 1 < new_floor < self.number_of_floors:
+    def go_to(self, new_floor):                             # 4
+        if 1 <= new_floor <= self.number_of_floors:
             for i in range(1, new_floor + 1):
-                print(i)
-            else:
-                print('Такого этажа не существует')
+                print (i)
+        else:
+            print ('Такого этажа не существует')
+            # а по заданию надо поставить вместо 0 (или убрать) => 1
+
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
